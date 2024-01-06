b26034f647d8b4d0311b7eee3e1277c9cdeb118c
Welcome2ImtyGitHub <1mty4personal@gmail.com>
2024-01-06 12:23:58 -0500

    Added a new print statement

--- src/com/smart/tests/Test.java
+++ src/com/smart/tests/Test.java
@@ -1,5 +1,13 @@
 package com.smart.tests;
 
-public class Test {
+public class Test 
+{
+	@org.testng.annotations.Test
+	public void loginTest()
+	{
+		System.out.println("We are learning Git and GitHub through Eclipse.");
+		System.out.println("We had our initial commit.");
+		
+	}
 
 }
